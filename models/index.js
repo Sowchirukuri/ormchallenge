39c3af847e1f17ede1e8da1e8dc284d803c6f6ab
Sowchirukuri <lakshmisowmya.chirukuri@gmail.com>
2023-10-02 12:42:07 -0500

    set up associations

--- models/index.js
+++ models/index.js
@@ -1,16 +1,32 @@
-// import models
+// Import models
 const Product = require('./Product');
 const Category = require('./Category');
 const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
-// Products belongsTo Category
+// Set up associations
 
-// Categories have many Products
+// Each Product belongs to a Category
+Product.belongsTo(Category, {
+  foreignKey: 'category_id', // Define the foreign key as 'category_id' in the Product model
+});
 
-// Products belongToMany Tags (through ProductTag)
+// Each Category can have many Products
+Category.hasMany(Product, {
+  foreignKey: 'category_id', // Use the same foreign key 'category_id' in the Product model
+});
 
-// Tags belongToMany Products (through ProductTag)
+// Each Product can belong to many Tags through the ProductTag model
+Product.belongsToMany(Tag, {
+  through: ProductTag, // Define the intermediate model through which the association is made
+  foreignKey: 'product_id', // Set the foreign key in the intermediate model
+});
+
+// Each Tag can belong to many Products through the ProductTag model
+Tag.belongsToMany(Product, {
+  through: ProductTag, // Define the intermediate model through which the association is made                 
+  foreignKey: 'tag_id', // Set the foreign key in the intermediate model
+});
 
 module.exports = {
   Product,
