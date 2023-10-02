d956b1d72f795ba0a1ce6778945d18160ecc41ad
Sowchirukuri <lakshmisowmya.chirukuri@gmail.com>
2023-10-02 12:27:51 -0500

    define category model

--- models/Category.js
+++ models/Category.js
@@ -2,19 +2,31 @@ const { Model, DataTypes } = require('sequelize');
 
 const sequelize = require('../config/connection.js');
 
+// Define the Category model 
 class Category extends Model {}
 
+// Initialize the Category model 
 Category.init(
   {
-    // define columns
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,       // Set 'id' as the primary key
+      autoIncrement: true,    // Allow auto-incrementing of 'id'
+      allowNull: false,       // 'id' cannot be null
+    },
+    category_name: {
+      type: DataTypes.STRING, // Define 'category_name' as a string
+      allowNull: false,       // 'category_name' cannot be null
+    }
   },
   {
-    sequelize,
-    timestamps: false,
-    freezeTableName: true,
+    sequelize,       // Connect to the Sequelize instance
+    timestamps: false, // Disable timestamps
+    freezeTableName: true, 
     underscored: true,
-    modelName: 'category',
+    modelName: 'category', // Set the model name to category
   }
 );
 
+// export the model category
 module.exports = Category;
